0ffb8e802656bf49c9efe389168448b138fc5560
Khoi Geeky <khoi.geeky@gmail.com>
2014-11-02 20:59:33 +0700

    Version 0.1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var trapFuncs = require('../trapFuncs.js');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Hello',
+    word: 'Hello',
+    content: trapFuncs.generate('Hello')
+  });
 });
-
+router.get('/:word',function(req,res){
+  res.render('index',{
+    title: req.params.word,
+    word: req.params.word,
+    content: trapFuncs.generate(req.params.word)
+  })
+});
 module.exports = router;
---
+++ trapFuncs.js
@@ -0,0 +1,26 @@
+var trapFuncs = {};
+var fs = require('fs');
+trapFuncs.words = fs.readFileSync('vnmwords.txt').toString().split("\n");
+trapFuncs.addLink = function(html) {
+    var rand = require('random-seed').create(html);
+    if (rand(10) === 1) {
+        html = "<a href='" + html + "'>" + html + "</a>"; // add link every about ten words.
+    }
+    if (rand(100) === 1) {
+        html += "<br>"; // Add new line every about 100 words
+    } else if (rand(200) === 1) {
+        var header = rand.intBetween(1, 3);
+        html = "</p><h" + header + ">" + html + "</h" + header + "><p>";// add new paragraph every about 60 words
+    }
+    return html;
+}
+trapFuncs.generate = function(word){
+    var html = "";
+    var rand2 = require('random-seed').create(word);
+    for (var i = 0 ; i < rand2.intBetween(400,600); i++){
+        html = html + ' ' + this.addLink(this.words[rand2(this.words.length)]);
+    }
+    return html;
+
+}
+module.exports = trapFuncs;
